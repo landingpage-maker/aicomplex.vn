10feb5704733f4cee9300c507c6022b6a7adc287
HarryDuc <vuminhduc.contact@gmail.com>
2026-04-12 12:23:42 +0700

    Change text button

--- components/hero.js
+++ components/hero.js
@@ -81,7 +81,7 @@ export default function Hero(targetId = "app") {
               </button>
               <button class="bg-red-600 text-white px-4 md:px-6 py-3 md:py-4 rounded-lg text-sm md:text-base font-semibold hover:bg-red-700 transition-colors">
                 <a href="https://upscale.aicomplex.vn/" target="_blank" rel="noopener noreferrer">
-                Interior/ upscale
+                Render - Upscale
                 </a>
               </button>
               <button onclick="window.open('https://www.youtube.com/@NguyenKhanh-mp6xg', '_blank')" class="bg-red-600 text-white px-4 md:px-6 py-3 md:py-4 rounded-lg text-sm md:text-base font-semibold hover:bg-red-700 transition-colors">
